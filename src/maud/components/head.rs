8aa44cd9cbba4b616c975a4a9ff2e849a71946c4
Tcode <kkrakenbreaker@gmail.com>
2026-03-24 19:11:36 -0500

    Fix head js

--- src/maud/components/head.rs
+++ src/maud/components/head.rs
@@ -3,6 +3,7 @@ use maud::{html, Markup, DOCTYPE};
 pub fn head_component(title: &str, css_version: Option<String>) -> Markup {
 
     let css = "/public/css/css.prod.css";
+    let common_js = format!("/public/js/common{}.js", if css_version.is_some() { css_version.clone().unwrap() } else { ".prod".to_string() });
 
     html! {
         (DOCTYPE)
@@ -10,9 +11,9 @@ pub fn head_component(title: &str, css_version: Option<String>) -> Markup {
             head {
                 meta charset="UTF-8";
                 meta name="viewport" content="width=device-width, initial-scale=1.0";
-                link rel="stylesheet" href=(format!("{}{}", css, css_version.clone().unwrap_or_default()));
+                link rel="stylesheet" href=(format!("{}{}", css, css_version.unwrap_or_default()));
                 link rel="icon" href="/public/images/favicon.ico" type="image/x-icon";
-                script src=(format!("/public/js/common.js{}", css_version.unwrap_or_default())) {}
+                script src=(common_js) {}
                 title {
                     (title)
                 }
