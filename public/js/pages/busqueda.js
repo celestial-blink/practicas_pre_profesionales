3ac4fa3ec0d9e266c87da75252888ded2763d822
Tcode <kkrakenbreaker@gmail.com>
2026-05-10 23:05:57 -0500

    Advanges

--- public/js/pages/busqueda.js
+++ public/js/pages/busqueda.js
@@ -1,9 +1,66 @@
-const params = new URLSearchParams(window.location.search);
+const query_params = new URLSearchParams(window.location.search);
 
 // escribe en params el valor del select de departamento
 const set_params = (key = '', value) => {
     const allow_keys = ['departamento', 'modalidad', 'nivel_academico', 'organizacion'];
     if (allow_keys.includes(key)) {
-        params.set(key, value);
+        query_params.set(key, value);
     }
 }
+
+const query_params_set_init = () => {
+    const query_params_elements = document.querySelectorAll('[data-ref="query_params"]');
+    query_params_elements.forEach(element => {
+        if (element.type === 'checkbox') {
+            element.checked = query_params.get(element.name) === element.value;
+        }
+    });
+};
+
+const handle_search_org_focus = () => {
+    const search_org_list = document.getElementById('search_org_list');
+    search_org_list.classList.replace('hidden', 'flex');
+}
+
+// solo si org_selected_container no tiene mas de 3 items
+const handle_set_org_item = (event) => {
+    const org_selected_container = document.getElementById('org_selected_container');
+    if (org_selected_container.children.length >= 3) {
+        event.preventDefault();
+        return;
+    }
+    const search_org_list = document.getElementById('search_org_list');
+    search_org_list.classList.replace('flex', 'hidden');
+    const prepare_item = document.createElement('span');
+    prepare_item.className = 'flex gap-1 text-sm items-center bg-rose-950 px-2 rounded-full text-rose-200 group-hover:text-white transition';
+    prepare_item.innerHTML = `
+        ${event.target.textContent}
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M18 6l-12 12" />
+            <path d="M6 6l12 12" />
+        </svg>
+    `;
+    org_selected_container.appendChild(prepare_item);
+    // elimina el item seleccionado de la lista
+    event.target.remove();
+}
+
+const handle_unset_org_item = (event) => {
+    const span_child_element = event.target.closest('span');
+    const org_selected_container = document.getElementById('org_selected_container');
+    org_selected_container.removeChild(span_child_element);
+}
+
+// quita el foco del input de busqueda de organizaciones
+document.addEventListener('click', (event) => {
+    const search_org_container = document.getElementById('search_org_container');
+    if (!search_org_container.contains(event.target)) {
+        const search_org_list = document.getElementById('search_org_list');
+        search_org_list.classList.replace('flex', 'hidden');
+    }
+});
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     query_params_set_init();
+// });
--- src/maud/pages/busqueda/aside_filters.rs
+++ src/maud/pages/busqueda/aside_filters.rs
@@ -6,17 +6,66 @@ pub fn aside_filters() -> Markup {
             div class="space-y-8" {
                 div {
                     h3 class="text-sm font-bold text-slate-300 uppercase tracking-widest mb-4" {
+                        "Organización (Máximo 3)"
+                    }
+                    div class="space-y-3" id="search_org_container" {
+                        input type="hidden" name="organizacion" data-ref="query_params";
+                        div class="flex items-center flex-wrap gap-2 cursor-pointer group" id="org_selected_container" onclick="handle_unset_org_item(event)" { }
+                        input
+                            type="text"
+                            class="w-full bg-slate-900 border border-slate-700/50 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-rose-500 transition"
+                            name="search"
+                            placeholder="Buscar organización..."
+                            autocomplete="off"
+                            onfocus="handle_search_org_focus(event)";
+
+                        menu class="hidden flex-col border border-slate-700/50 rounded-xl p-3 bg-slate-900" id="search_org_list" {
+                            li {
+                                button type="button" class="flex items-center cursor-pointer text-slate-400 hover:text-white transition w-full p-1" onclick="handle_set_org_item(event)" {
+                                    "Organizacion 1"
+                                }
+                            }
+                            li {
+                                button type="button" class="flex items-center cursor-pointer text-slate-400 hover:text-white transition w-full p-1" onclick="handle_set_org_item(event)" {
+                                    "Organizacion 2"
+                                }
+                            }
+                            li {
+                                button type="button" class="flex items-center cursor-pointer text-slate-400 hover:text-white transition w-full p-1" onclick="handle_set_org_item(event)" {
+                                    "Organizacion 3"
+                                }
+                            }
+                            li {
+                                button type="button" class="flex items-center cursor-pointer text-slate-400 hover:text-white transition w-full p-1" onclick="handle_set_org_item(event)" {
+                                    "Organizacion 4"
+                                }
+                            }
+                            li {
+                                button type="button" class="flex items-center cursor-pointer text-slate-400 hover:text-white transition w-full p-1" onclick="handle_set_org_item(event)" {
+                                    "Organizacion 5"
+                                }
+                            }
+                            li {
+                                button type="button" class="flex items-center cursor-pointer text-slate-400 hover:text-white transition w-full p-1" onclick="handle_set_org_item(event)" {
+                                    "Organizacion 5"
+                                }
+                            }
+                        }
+                    }
+                }
+                div {
+                    h3 class="text-sm font-bold text-slate-300 uppercase tracking-widest mb-4" {
                         "Modalidad de prácticas"
                     }
                     div class="space-y-3" {
                         label class="flex items-center space-x-3 cursor-pointer group" {
-                            input type="checkbox" class="w-4 h-4 rounded border-slate-700";
+                            input type="checkbox" class="w-4 h-4 rounded border-slate-700" data-ref="query_params";
                             span class="text-slate-400 group-hover:text-white transition" {
                                 "Pre profesionales"
                             }
                         }
                         label class="flex items-center space-x-3 cursor-pointer group" {
-                            input type="checkbox" class="w-4 h-4 rounded border-slate-700";
+                            input type="checkbox" class="w-4 h-4 rounded border-slate-700" data-ref="query_params";
                             span class="text-slate-400 group-hover:text-white transition" {
                                 "Profesionales"
                             }
@@ -29,46 +78,33 @@ pub fn aside_filters() -> Markup {
                     }
                     div class="space-y-3" {
                         label class="flex items-center space-x-3 cursor-pointer group" {
-                            input type="checkbox" class="w-4 h-4 rounded border-slate-700";
+                            input type="checkbox" class="w-4 h-4 rounded border-slate-700" name="nivel_academico" value="1" data-ref="query_params";
                             span class="text-slate-400 group-hover:text-white transition" {
                                 "Estudiantes técnicos"
                             }
                         }
                         label class="flex items-center space-x-3 cursor-pointer group" {
-                            input type="checkbox" class="w-4 h-4 rounded border-slate-700";
+                            input type="checkbox" class="w-4 h-4 rounded border-slate-700" name="nivel_academico" value="2" data-ref="query_params";
                             span class="text-slate-400 group-hover:text-white transition" {
                                 "Egresados técnicos"
                             }
                         }
                         label class="flex items-center space-x-3 cursor-pointer group" {
-                            input type="checkbox" class="w-4 h-4 rounded border-slate-700";
+                            input type="checkbox" class="w-4 h-4 rounded border-slate-700" name="nivel_academico" value="3" data-ref="query_params";
                             span class="text-slate-400 group-hover:text-white transition" {
                                 "Estudiantes universitarios"
                             }
                         }
                         label class="flex items-center space-x-3 cursor-pointer group" {
-                            input type="checkbox" class="w-4 h-4 rounded border-slate-700";
+                            input type="checkbox" class="w-4 h-4 rounded border-slate-700" name="nivel_academico" value="4" data-ref="query_params";
                             span class="text-slate-400 group-hover:text-white transition" {
                                 "Egresados universitarios"
                             }
                         }
                         label class="flex items-center space-x-3 cursor-pointer group" {
-                            input type="checkbox" class="w-4 h-4 rounded border-slate-700";
+                            input type="checkbox" class="w-4 h-4 rounded border-slate-700" name="nivel_academico" value="5" data-ref="query_params";
                             span class="text-slate-400 group-hover:text-white transition" {
                                 "Bachilleres"
-                            }
-                        }
-                    }
-                }
-                div {
-                    h3 class="text-sm font-bold text-slate-300 uppercase tracking-widest mb-4" {
-                        "Organización"
-                    }
-                    div class="space-y-3" {
-                        label class="flex items-center space-x-3 cursor-pointer group" {
-                            input type="checkbox" class="w-4 h-4 rounded border-slate-700";
-                            span class="text-slate-400 group-hover:text-white transition" {
-                                "SUNARP"
                             }
                         }
                     }
